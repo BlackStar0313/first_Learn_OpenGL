5ba156228deb07af8ad0f8446b976fa7de6e2c10
zhiwei.liu <270274978@qq.com>
2016-07-27 11:36:55 +0800

    增加第一个OpenGL工程和第一个程序

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # first_Learn_OpenGL
 here is my first time to learn OpenGL, and write down all the test sample~~ Hope i can get it!
+
+
+create branch
---
+++ learn_OpenGL/learn_OpenGL/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  first_GL
+//
+//  Created by liuzhiwei on 16/7/27.
+//  Copyright © 2016年 liuzhiwei. All rights reserved.
+//
+
+#include <iostream>
+
+#include <GLUT/GLUT.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(-0.5, 0.5);
+    glVertex2f(0.5, 0.5);
+    glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char * argv[]) {
+    // insert code here...
+//    std::cout << "Hello, World!\n";
+    glutInit(&argc, argv);
+    glutCreateWindow("Xcide glut demo ");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
